d7cf131bf1154d54524943188e443f29b9a2bb4a
Jacob Berg <coffeesnakes@gmail.com>
2020-10-29 17:44:13 -0700

    API call to randomuser created

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// vars to access dom elements/buttons
+const main = document.getElementById('main');
+const addUserButton = document.getElementById('add-user');
+const doubleButton = document.getElementById('double');
+const showMillionaires = document.getElementById('show-millionaires');
+const sortButton = document.getElementById('sort');
+const calculateWealthButton = document.getElementById('calculate-wealth');
